eced4c42340cff4a7c476078431262111ab97373
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-18 16:16:09 +0000

    final

--- umbrella.rb
+++ umbrella.rb
@@ -14,15 +14,30 @@ location = gets
 gmaps_api_key = ENV["GMAPS_KEY"]
 gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location.gsub(" ","%20").gsub("\n","") + "&key=" + gmaps_api_key
 
-raw_response = HTTP.get(gmaps_url)
-parsed_response = JSON.parse(raw_response)
+raw_response_maps = HTTP.get(gmaps_url)
+parsed_response = JSON.parse(raw_response_maps)
 
 # Extracting the latitude and longitude info from API response
-results = parsed_response.fetch("results")
-geometry = results.at(0).fetch("geometry")
-location = geometry.fetch("location")
-lat = location.fetch("lat")
-long = location.fetch("lng")
+results_array = parsed_response.fetch("results")
+geometry_hash = results_array.at(0).fetch("geometry")
+location_hash = geometry_hash.fetch("location")
+lat = location_hash.fetch("lat")
+long = location_hash.fetch("lng")
 
 #Getting weather info from Pirate Weather API
+pirate_weather_api_key = ENV["PIRATE_WEATHER_API_KEY"]
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/" + lat.to_s + "," + long.to_s
 
+raw_response_weather = HTTP.get(pirate_weather_url)
+parsed_response = JSON.parse(raw_response_weather)
+
+# Extracting the precipitation volume from API response
+currently_hash = parsed_response.fetch("currently")
+
+current_rain = currently_hash.fetch("precipType")
+
+if current_rain == "rain"
+  puts "Take your umbrella!"
+else
+  puts "You're fine without an umbrella!"
+end
